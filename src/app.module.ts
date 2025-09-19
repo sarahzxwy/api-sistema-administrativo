e56a01046dbbeb41bc0ce2168a99026167a6afa1
sarahzxwy <sarahalvesborges@outlook.com>
2025-09-19 14:39:39 -0300

    feat: database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 import { RoleModule } from './role/role.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, RoleModule],
+  imports: [UserModule, RoleModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
