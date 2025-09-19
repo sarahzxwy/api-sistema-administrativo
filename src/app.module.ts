d2214c0365e19b83c98f4eb2488bab4fcdaccefc
sarahzxwy <sarahalvesborges@outlook.com>
2025-09-19 14:35:48 -0300

    feat: role resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
