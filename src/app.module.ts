eada579a6c35ae9b3ef09db7008ba827a48015ae
sarahzxwy <sarahalvesborges@outlook.com>
2025-11-07 13:03:06 -0300

    feat: task module to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { RoleModule } from './role/role.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ProjectModule } from './project/project.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [UserModule, RoleModule, DatabaseModule, AuthModule, ProjectModule],
+  imports: [UserModule, RoleModule, DatabaseModule, AuthModule, ProjectModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
